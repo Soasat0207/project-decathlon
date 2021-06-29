d0d504d97ca2e7e9c73583440dd05ff15f130dc6
Vu Long <vulongpt@gmail.com>
2021-06-29 07:43:03 +0700

    update cart.html

--- server.js
+++ server.js
@@ -2,13 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
-const productRouter = require('./router/productRouter');
-const categoryRouter = require('./router/categoryRouter');
-const colorRouter = require('./router/colorProductRouter');
-const levelRouter = require('./router/levelProductRouter');
-const sizeRouter = require('./router/sizeProductRouter');
-const supplierRouter = require('./router/supplierRouter');
-const trademarkRouter = require('./router/trademarkRouter');
+
 
 // parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }))
@@ -38,18 +32,15 @@ app.get('/admin-list-color', (req, res) => {
 app.get('/test3', (req, res) => {
   res.sendFile(path.join(__dirname,'./view/test3.html'))
 })
+app.get('/cart', (req, res, next) => {
+  res.sendFile(path.join(__dirname, './view/cart.html'))
+})
 
 // make static link
 app.use('/public',express.static(path.join(__dirname, './public')));
 
 // Use router
-app.use('/api/product',productRouter);
-app.use('/api/category',categoryRouter);
-app.use('/api/color',colorRouter);
-app.use('/api/level',levelRouter);
-app.use('/api/size',sizeRouter);
-app.use('/api/supplier',supplierRouter);
-app.use('/api/trademark',trademarkRouter);
+
 
 // Port to listen
 app.listen(port, () => {
